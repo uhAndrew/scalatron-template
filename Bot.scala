5c979f9a6e984b3ab2a1df5e88d67d3daf6c7e98
Andrew Gray <andrew.gray@hootsuite.com>
2014-03-27 20:05:01 -0700

    more noodling

--- Bot.scala
+++ Bot.scala
@@ -7,22 +7,70 @@ case class Direction(val x:Int, val y:Int) {
   override def toString = "direction=" + x + ":" + y
 }
 
-class Bot {
-  def respond(input: String) = {
-    //"Status(text=Hello World)"
-    println(CommandParser(input).toString)
-    //log(CommandParser(input).toString)
-    move(Direction(1,1))
-  }
+case class Position(val x:Int, val y:Int) {
+  override def toString = "position=" + x + ":" + y
+}
 
+case class BotProperty(val k:String, val v: String) {
+  override def toString = k + "=" + v
+}
+
+//case class StateValue, 
+//Set(key=value,...)
+
+trait BotUtils {
   def log(str:String) = "Log(text=" + str + ")"
-
   def move(x:Int, y:Int):String = "Move(direction=" + x + ":" + y + ")"
   def move(dir:Direction):String = dir match {
     case Direction(x,y) => move(x,y)
   }
 
   def spawn(dir:Direction, name:String, energy:Int) = "Spawn(" + dir.toString + ",name=" + name + ",energy=" + energy + ")"
+
+  // haha abusing Set.toString to get Set(....)
+  def setKV(kvs:Set[BotProperty]) = kvs.toString
+
+  def explode(s:Int) = "Explode(" + s + ")"
+
+  def say(s:String) = "Say(" + s + ")"
+
+  def status(s:String) = "Status(" + s + ")"
+
+  //MarkCell(position=int:int,color=string)
+  def markcell(p:Position, color:String) = "MarkCell(" + p + "," + color + ")"
+}
+
+class Bot extends BotUtils {
+  def respond(input: String) = {
+    //"Status(text=Hello World)"
+    val inputmap = CommandParser(input)
+
+    //println("map=" + inputmap)
+    //log(CommandParser(input).toString)
+
+    inputmap match {
+      case ("Welcome", _) => {
+        val kvset = Set(BotProperty("one", "one!"), BotProperty("two", "two!"))
+        println(setKV(kvset))
+        ""
+      }
+      case ("React", m:Map[String,String]) => react(m)
+      case (str, m:Map[String,String]) => move(Direction(-1,-1))
+    }
+
+  }
+
+  def react(m:Map[String,String]) = {
+    //println("React")
+    move(Direction(1,1))
+  }
+
+}
+
+object SlaveBot extends BotUtils {
+  def react(m:Map[String, String]) = {
+    move(Direction(1,1))
+  }
 }
 
 /** Utility methods for parsing strings containing a single command of the format
